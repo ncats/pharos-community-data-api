ff1c338db4ed6275c764160b226324b221ef0f04
KeithKelleher <keith.kelleher@ncats.nih.gov>
2023-10-13 15:15:54 -0400

    Deidre's changes and framework updates

--- app.ts
+++ app.ts
@@ -3,6 +3,9 @@ import express, {Application, Request, Response} from 'express';
 import {ping, sample} from "./models/endpoints";
 // @ts-ignore
 import {predictions} from "./data_sources/kinase-cancer-predictions/index"
+import {interactorScores} from "./data_sources/pairwise-relationship-data";
+import {coexpressionData} from "./data_sources/coexpression-data";
+
 const app: Application = express()
 
 const port: number = 3001
@@ -12,6 +15,10 @@ app.get('/ping*', (req: Request, res: Response) => ping(req, res));
 app.get("/sample?*", (req: Request, res: Response) => sample(req, res));
 
 app.get("/predictions?*", (req: Request, res: Response) => predictions(req, res));
+
+app.get("/interactorScores?*", (req: Request, res: Response) => interactorScores(req, res));
+
+app.get("/coexpressionData?*", (req: Request, res: Response) => coexpressionData(req, res));
 
 app.listen(port, function () {
     console.log(`App is listening on port ${port} !`)
